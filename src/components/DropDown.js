8b1c1d9051b78112cfbc8cf4f89500bc0ae077f9
Alex Salazar <alxanderapollo@gmail.com>
2021-06-26 15:51:35 -0400

    added functionality that allows the user to click outside of the drop down menu and close

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,7 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect,useRef} from 'react';
 export default function DropDown({options, selected, onSelectedChange}) {
     //piece of state responsible for toggling the drop down menu
     const [open,setOpen] = useState(false);
+    const ref = useRef();
+
+    //we are adding a useEffect listener to listen for elements clicked outside of the drop down menu
+    useEffect(() =>{
+        //listening for a onClick event
+        document.body.addEventListener('click',(event)=>{
+        //ref.current.contains (contains specfically belongs to all dom elements) will see if whether or not the item we clicked on is inside of our component
+        if (ref.current.contains(event.target)){
+            return;
+        }
+        //otherwise we should close the drop down menu
+        setOpen(false);
+
+        //console.log(event.target)//refrence the specfic dom element that was specfically clicked
+        //   console.log("Body Clicked")
+        
+        });
+
+    },[]);
+
     
 
     const renderedOptions = options.map(option =>{
@@ -11,29 +31,41 @@ export default function DropDown({options, selected, onSelectedChange}) {
         // our key is the option value
         // the key is the value so red, green or blue
        {/*Then the label that is being printed is the one that was created in the previous page */}
+       //items 
     return (
     
         <div
             key={option.value} 
             className="item"
-            onClick={() => onSelectedChange(option)}
+            onClick={() => {
+                // console.log("Item Clicked")
+                onSelectedChange(option)
+            }}
         >
         {option.label}
         </div>
     );
 });
+
+
     //visible  active and visiable transition
     //inside of the div, these two things are responsible for whether the drop down closes 
     //or not so for us selection drop down, using the above peice of state we are saying, if its open keep the menu showing, otherwise close it and
     //for menu if we are still looking at oprions keep it visiable otherwise close the menu
     //will be using a piece of state to control when to toggle the drop down menu and when to not
     //!open means to set the opposite
+    //! ui form is our most parent component which is why it contains useRef
+    //console.log('my ref' + ref.current); //get a refrence to that 
     return (
-        <div className="ui form">
+        <div ref  = {ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 {/* if open is true set to visiable active meaning drop down the menus other wise close */}
-                <div onClick={() => setOpen(!open)}className={`ui selection dropdown ${open ?'visible active': ''}`}>
+                <div onClick={() => {
+                    // console.log("DropDown Clicked");
+                      setOpen(!open);
+                }}
+                className={`ui selection dropdown ${open ?'visible active': ''}`}>
                 <i className="dropdown icon"></i>
                 <div className="text"> {selected.label}</div>
                 <div className={`menu ${open ? 'visible transition': ''}`}>
