a07ceef935cd5dc8a807b00c4d4fdc4c2c6c18cf
Alex Salazar <alxanderapollo@gmail.com>
2021-06-27 15:50:48 -0400

    added the fix, going between pages doesnt automatically request the api now =)

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,14 +2,19 @@
 //the point of this is that before with the navigation bar, each page reload would request information
 //this could be a problem if you're constantly pulling from an API 
 //so now that event.PreventDefault is written into the code editor there isnt that problem
-//now we should be able to change the url without causing a page refresh
+//now we should be able to change the url without causing a page refresh and we do that by using the window.history.pushState({},'',href);
 import React from 'react';
 
-const onClick =( event)=>{
-    // prevents a full page reload
-    event.preventDefault();
-};
 //takes the class name, a link to the routing page 
 export default function Link({className,href, children}) {
+    const onClick =( event)=>{
+        // prevents a full page reload
+        event.preventDefault();
+        //will allow us to change url's without  the need of refreshing a page
+        window.history.pushState({},'',href);
+        // the next two lines of code will communicate to react that the url just changed
+        const navEvent = new PopStateEvent('popstate')
+        window.dispatchEvent(navEvent)
+    };
     return <a onClick= {onClick}className= {className} href={href}>{children}</a>
 }
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,8 +1,30 @@
+import {useEffect, useState} from 'react';
+
 
 //takes the path and children
 //sets the route for each of the pages
 export default function Route({path, children}) {
-    return  window.location.pathname === path 
-    ? children 
-    : null;
+    //this piece of state only exists go get our route path update
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+
+        const onLocationChange = (event) => {
+            //the value gets updated to the value of whatever current path the user is currently on
+            setCurrentPath(window.location.pathname)
+            
+        };
+  
+        window.addEventListener('popstate',onLocationChange)
+
+        //clean up function to remove addEventListener
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        };
+
+    //want this function to run only once
+    },[]);
+
+
+    return  currentPath === path ? children : null;
 }
